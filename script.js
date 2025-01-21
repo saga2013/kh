55955c175be4008c13c3edf58628e58d74ae92f5
saga2013 <saidakbar20132013@gmail.com>
2025-01-21 17:55:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,298 @@
+function bye(){
+    let p=prompt( `Qiyqiriq set nechta zakaz qilmoqchisiz`);
+    switch(p) {
+        case "1":
+        let o =45000
+        confirm( `Уверен? 1 шт стои ${o}`)
+            break;
+        case "2":
+            let n =45000*2
+            confirm( `Уверен? 2 шт стоит ${n}`)
+            break;
+        case "3":
+            let b =45000*3
+            confirm( `Уверен? 3 шт стоит ${b}`)
+            break;
+        case "4":
+            let f = 45000*4
+            confirm( `Уверен? 4шт стоит ${f}`)
+            break;
+        case "5":
+            let i = 45000*5
+            confirm( `Уверен? 5 шт стоит ${i}`)
+            break;
+        default:
+            alert(`Меньше пяти шт`)
+    }
+}
+function bye1(){
+    let p=prompt( `Снек сет nechta zakaz qilmoqchisiz`);
+    switch(p) {
+        case "1":
+        let o =79000
+        confirm( `Уверен? 1 шт стоит ${o}`)
+            break;
+        case "2":
+            let n =79000*2
+            confirm( `Уверен? 2 шт стоит ${n}`)
+            break;
+        case "3":
+            let b =79000*3
+            confirm( `Уверен? 3 шт стоит ${b}`)
+            break;
+        case "4":
+            let f = 79000*4
+            confirm( `Уверен? 4 шт стоит ${f}`)
+            break;
+        case "5":
+            let i = 79000*5
+            confirm( `Уверен? 5 шт стоит ${i}`)
+            break;
+        default:
+            alert(`Меньше пяти шт`)
+    }
+}
+
+function bye2(){
+    let p=prompt( `СКлассик сет nechta zakaz qilmoqchisiz`);
+    switch(p) {
+        case "1":
+        let o =34000
+        confirm( `Уверен? 1 шт стоит ${o}`)
+            break;
+        case "2":
+            let n =34000*2
+            confirm( `Уверен? 2 шт стоит ${n}`)
+            break;
+        case "3":
+            let b =34000*3
+            confirm( `Уверен? 3 шт стоит ${b}`)
+            break;
+        case "4":
+            let f = 34000*4
+            confirm( `Уверен? 4 шт стоит ${f}`)
+            break;
+        case "5":
+            let i = 34000*5
+            confirm( `Уверен? 5 шт стоит ${i}`)
+            break;
+        default:
+            alert(`Меньше пяти шт`)
+    }
+}
+function bye3(){
+    let p=prompt( `Лонгер рингс сет nechta zakaz qilmoqchisiz`);
+    switch(p) {
+        case "1":
+        let o =79000
+        confirm( `Уверен? 1 шт стоит ${o}`)
+            break;
+        case "2":
+            let n =79000*2
+            confirm( `Уверен? 2 шт стоит ${n}`)
+            break;
+        case "3":
+            let b =79000*3
+            confirm(` Уверен? 3 шт стоит ${b}`)
+            break;
+        case "4":
+            let f = 79000*4
+            confirm( `Уверен? 4 шт стоит ${f}`)
+            break;
+        case "5":
+            let i = 79000*5
+            confirm(` Уверен? 5 шт стоит ${i}`)
+            break;
+        default:
+            alert(`Меньше пяти шт`)
+    }
+}
+function bye4(){
+    let p=prompt(`Биг сет nechta zakaz qilmoqchisiz`);
+    switch(p) {
+        case "1":
+        let o =62000
+        confirm( `Уверен? 1 шт стоит ${o}`)
+            break;
+        case "2":
+            let n =62000*2
+            confirm(` Уверен? 2 шт стоит ${n}`)
+            break;
+        case "3":
+            let b =62000*3
+            confirm( `Уверен? 3 шт стоит ${b}`)
+            break;
+        case "4":
+            let f = 62000*4
+            confirm(` Уверен? 4 шт стоит ${f}`)
+            break;
+        case "5":
+            let i = 62000*5
+            confirm(` Уверен? 5 шт стоит ${i}`)
+            break;
+        default:
+            alert(`Меньше пяти шт`)
+    }
+}
+function bye5(){
+    let p=prompt(` Лестер сет nechta zakaz qilmoqchisiz`);
+    switch(p) {
+        case "1":
+        let o =53000
+        confirm( `Уверен? 1 шт стоит ${o}`)
+            break;
+        case "2":
+            let n =53000*2
+            confirm(` Уверен? 2 шт стоит ${n}`)
+            break;
+        case "3":
+            let b =53000*3
+            confirm(` Уверен? 3 шт стоит ${b}`)
+            break;
+        case "4":
+            let f = 53000*4
+            confirm( `Уверен? 4 шт стоит ${f}`)
+            break;
+        case "5":
+            let i = 53000*5
+            confirm( `Уверен? 5 шт стоит ${i}`)
+            break;
+            default:
+            alert(`Меньше пяти шт`)
+    }
+}
+function bye6(){
+    let p=prompt( `Скул сет nechta zakaz qilmoqchisiz`);
+    switch(p) {
+        case "1":
+        let o =36000
+        confirm(` Уверен? 1 шт стоит ${o}`)
+            break;
+        case "2":
+            let n =36000*2
+            confirm(` Уверен? 2 шт стоит ${n}`)
+            break;
+        case "3":
+            let b =36000*3
+            confirm( `Уверен? 3 шт стоит ${b}`)
+            break;
+        case "4":
+            let f = 36000*4
+            confirm(` Уверен? 4 шт стоит ${f}`)
+            break;
+        case "5":
+            let i = 36000*5
+            confirm( `Уверен? 5 шт стоит ${i}`)
+            break;
+        default:
+            alert(`Меньше пяти шт`)
+    }
+}
+function bye7(){
+    let p=prompt( `комбо сет nechta zakaz qilmoqchisiz`);
+    switch(p) {
+        case "1":
+        let o =20000
+        confirm( `Уверен? 1 шт стоит ${o}`)
+            break;
+        case "2":
+            let n =20000*2
+            confirm(` Уверен? 2 шт стоит ${n}`)
+            break;
+        case "3":
+            let b =20000*3
+            confirm( `Уверен? 3 шт стоит ${b}`)
+            break;
+        case "4":
+            let f = 20000*4
+            confirm( `Уверен? 4 шт стоит ${f}`)
+            break;
+        case "5":
+            let i = 20000*5
+            confirm( `Уверен? 5 шт стоит ${i}`)
+            break;
+        default:
+            alert(`Меньше пяти шт`)
+    }
+}
+function bye8(){
+    let p=prompt(` Снек сет nechta zakaz qilmoqchisiz`);
+    switch(p) {
+        case "1":
+        let o =43000
+        confirm( `Уверен? 1 шт стоит ${o}`)
+            break;
+        case "2":
+            let n =43000*2
+            confirm( `Уверен? 2 шт стоит ${n}`)
+            break;
+        case "3":
+            let b =43000*3
+            confirm( `Уверен? 3 шт стоит ${b}`)
+            break;
+        case "4":
+            let f = 43000*4
+            confirm( `Уверен? 4 шт стоит ${f}`)
+            break;
+        case "5":
+            let i = 43000*5
+            confirm(` Уверен? 5 шт стоит ${i}`)
+            break;
+        default:
+            alert(`Меньше пяти шт`)
+    }
+}
+function bye9(){
+    let p=prompt(`Do’stlar 2х nechta zakaz qilmoqchisiz`);
+    switch(p) {
+        case "1":
+        let o =45000
+        confirm( `Уверен? 1 шт стоит ${o}`)
+            break;
+        case "2":
+            let n =45000*2
+            confirm( `Уверен? 2 шт стоит ${n}`)
+            break;
+        case "3":
+            let b =45000*3
+            confirm( `Уверен? 3 шт стоит ${b}`)
+            break;
+        case "4":
+            let f = 45000*4
+            confirm( `Уверен? 4 шт стоит ${f}`)
+            break;
+        case "5":
+            let i = 45000*5
+            confirm( `Уверен? 5 шт стоит ${i}`)
+            break;
+        default:
+            alert(`Меньше пяти шт`)
+    }
+}
+function bye10(){
+    let p=prompt(`Острый Do’stlar 1х nechta zakaz qilmoqchisiz`);
+    switch(p) {
+        case "1":
+        let o =43000
+        confirm( `Уверен? 1 шт стоит ${o}`)
+            break;
+        case "2":
+            let n =43000*2
+            confirm( `Уверен? 2 шт стоит ${n}`)
+            break;
+        case "3":
+            let b =43000*3
+            confirm( `Уверен? 3 шт стоит ${b}`)
+            break;
+        case "4":
+            let f = 43000*4
+            confirm(`Уверен? 4 шт стоит ${f}`)
+            break;
+        case "5":
+            let i = 43000*5
+            confirm( `Уверен? 5 шт стоит ${i}`)
+            break;
+        default:
+            alert(`Меньше пяти шт`)
+    }
+}
